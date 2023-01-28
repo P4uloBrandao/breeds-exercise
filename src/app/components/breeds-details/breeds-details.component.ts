be63fb1f4e6e13e18e591cdc8c3da402b688d0f9
Paulo Brandão <pricardo2102@gmail.com>
2023-01-28 01:08:48 -0300

    create components

---
+++ src/app/components/breeds-details/breeds-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-breeds-details',
+  templateUrl: './breeds-details.component.html',
+  styleUrls: ['./breeds-details.component.scss']
+})
+export class BreedsDetailsComponent {
+
+}
---
+++ src/app/components/breeds-list/breeds-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-breeds-list',
+  templateUrl: './breeds-list.component.html',
+  styleUrls: ['./breeds-list.component.scss']
+})
+export class BreedsListComponent {
+
+}
