4dea2abc10b8fbc1b9af4056a3b52a02c4d29dfb
Paulo Brandão <pricardo2102@gmail.com>
2023-01-29 02:37:33 -0300

    finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { SharedModule } from "./shared/shared.module";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HttpClientModule } from "@angular/common/http";
 import { MatButtonModule } from "@angular/material/button";
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [AppComponent, BreedsListComponent, BreedsDetailsComponent],
@@ -21,6 +23,7 @@ import { MatButtonModule } from "@angular/material/button";
     HttpClientModule,
     MatButtonModule,
     MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/breeds-details/breeds-details.component.ts
+++ src/app/components/breeds-details/breeds-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
+import { GetService } from "src/app/shared/services/get.service";
 
 @Component({
   selector: "app-breeds-details",
@@ -10,9 +11,22 @@ export class BreedsDetailsComponent implements OnInit {
   public breed: any;
   public breedDetails: any;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private getFactService: GetService,
+    private router: Router
+  ) {
     this.breed = this.route.snapshot.queryParamMap.get("breed");
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getFactService.get("https://catfact.ninja/fact").subscribe((data) => {
+      this.breedDetails = data;
+    });
+  }
+
+  public back (){
+    this.router.navigate(['breeds-list']);
+
+  }
 }
